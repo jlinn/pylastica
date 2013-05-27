4d3ec52c81cf31e5501c4f52d2a5c174f26fef56
Christopher Guiney <chris@guiney.net>
2013-05-27 14:52:58 -0700

    Initial commit of setup scripts

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.txt').read()
+
+packages = find_packages(exclude=["tests", "tests.*"])
+
+setup(
+    name='pylastica',
+    version='0.1.0',
+    packages=packages,
+    url='https://bitbucket.org/jlinn5732/pylastica',
+    license='LICENSE.txt',
+    author='Joe Linn',
+    author_email='',
+    description="Python port of Nicolas Ruflin's Elastica PHP library",
+    long_description=long_description,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 2 - Pre-Alpha'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+    ]
+)
