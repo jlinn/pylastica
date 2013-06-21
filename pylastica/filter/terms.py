af51dd3cf58c15afc0b0c85940266e5a0a6e0fc9
Joe Linn <joe@venturocket.com>
2013-06-20 17:51:44 -0700

    Add set_lookup method to terms filter

--- pylastica/filter/terms.py
+++ pylastica/filter/terms.py
@@ -1,7 +1,8 @@
 __author__ = 'Joe Linn'
 
-#import pylastica.filter.abstractfilter
+import pylastica.exception
 from .abstractfilter import AbstractFilter
+
 
 class Terms(AbstractFilter):
     def __init__(self, key='', terms=None):
@@ -13,7 +14,8 @@ class Terms(AbstractFilter):
         @type terms: list of str
         """
         super(Terms, self).__init__()
-        self.set_terms(key, terms)
+        if terms is not None:
+            self.set_terms(key, terms)
 
     def set_terms(self, key, terms):
         """
@@ -29,6 +31,36 @@ class Terms(AbstractFilter):
             raise TypeError("terms must be a list: %r" % terms)
         self._key = key
         self._terms = terms
+        return self
+
+    def set_lookup(self, key, doc_type, id, path, index=None):
+        """
+        Set the lookup path for this terms filter
+        @param key: document field
+        @type key: str
+        @param doc_type: the document type from which to fetch the term values
+        @type doc_type: str or pylastica.doc_type.DocType
+        @param id: id of the document from which to fetch the term values
+        @type id: str
+        @param path: the field specified as path from which to fetch the values for the filter
+        @type path: str
+        @param index: The index from which to fetch the term values. Defaults to the current index.
+        @type index: str or pylastica.index.Index
+        @return:
+        @rtype: self
+        """
+        self._key = key
+        if isinstance(doc_type, pylastica.doc_type.DocType):
+            doc_type = doc_type.name
+        self._terms = {
+            'type': doc_type,
+            'id': id,
+            'path': path
+        }
+        if index is not None:
+            if isinstance(index, pylastica.index.Index):
+                index = index.name
+            self._terms['index'] = index
         return self
 
     def add_term(self, term):
